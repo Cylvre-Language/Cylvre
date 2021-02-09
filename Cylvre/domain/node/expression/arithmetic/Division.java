99a2d9e93f13e29580a691e38929df327eb354b5
SivAxis <76278244+SivAxis@users.noreply.github.com>
2021-02-09 18:53:08 +0530

    refactor typo in dir arithmetic

---
+++ Cylvre/domain/node/expression/arithmetic/Division.java
@@ -0,0 +1,21 @@
+package Cylvre.domain.node.expression.arithmetic;
+
+import CylvreCompiler.bytecodegeneration.expression.ExpressionGenerator;
+import CylvreCompiler.bytecodegeneration.statement.StatementGenerator;
+import Cylvre.domain.node.expression.Expression;
+
+public class Division extends ArithmeticExpression {
+    public Division(Expression leftExpress, Expression rightExpress) {
+        super(leftExpress,rightExpress);
+    }
+
+    @Override
+    public void accept(StatementGenerator generator) {
+        generator.generate(this);
+    }
+
+    @Override
+    public void accept(ExpressionGenerator generator) {
+        generator.generate(this);
+    }
+}
