cf86978fb68adda151d3682caa1954a0c2e5a096
CodeUnstable <76278244+CodeUnstable@users.noreply.github.com>
2021-03-01 18:47:09 +0530

    Delete src/main/java directory

--- src/main/java/Cylvre/exception/ClassNotFoundForNameException.java
+++
@@ -1,7 +0,0 @@
-package Cylvre.exception;
-
-public class ClassNotFoundForNameException extends RuntimeException {
-    public ClassNotFoundForNameException(String className) {
-        super("class not found " + className);
-    }
-}
--- src/main/java/Cylvre/exception/MixedComparisonNotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package Cylvre.exception;
-
-import Cylvre.domain.type.Type;
-
-public class MixedComparisonNotAllowedException extends RuntimeException {
-    public MixedComparisonNotAllowedException(Type leftType, Type rightType) {
-        super("Comparison between object and primitive is not supported  :" + leftType + "  |  " + rightType);
-    }
-}
