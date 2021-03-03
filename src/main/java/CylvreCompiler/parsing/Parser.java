8648cb3e10eb8356f802591a3d6fe9da7163e15e
CodeUnstable <76278244+CodeUnstable@users.noreply.github.com>
2021-03-03 07:20:01 +0530

    add error listener to updated compiler

---
+++ src/main/java/CylvreCompiler/parsing/Parser.java
@@ -0,0 +1,28 @@
+package CylvreCompiler.parsing;
+
+import Cylvre.domain.CompilationUnit;
+import CylvreCompiler.parsing.visitor.CompilationUnitVisitor;
+import gen.CylvreLexer;
+import gen.CylvreParser;
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Parser {
+    public CompilationUnit getCompilationUnit(String fileAbsolutePath) throws IOException {
+        CharStream charStream = CharStreams.fromPath(Paths.get(fileAbsolutePath));
+        CylvreLexer lexer = new CylvreLexer(charStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        CylvreParser parser = new CylvreParser(tokenStream);
+
+        ANTLRErrorListener errorListener = new CylvreErrorListener();
+        parser.addErrorListener(errorListener);
+
+        CompilationUnitVisitor compilationUnitVisitor = new CompilationUnitVisitor();
+        return parser.compilationUnit().accept(compilationUnitVisitor);
+    }
+}
